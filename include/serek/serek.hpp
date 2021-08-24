9545fda9038aad32e1dd6d508a6a08c2ad8dae22
raidgar98 <raidgar98@onet.pl>
2021-08-24 19:15:07 +0200

    :fire: Remove obsolete code

--- include/serek/serek.hpp
+++ include/serek/serek.hpp
@@ -4,19 +4,4 @@
 
 namespace serek
 {
-	// using field
-
-	struct ___null_t
-	{
-		/** @brief terminator */
-		bool accept(void*) { return false; }
-	};
-
-	/**
-	 * @brief serialization helper. all it does is to append null_t as member (which mean end of serialization)
-	*/
-	struct serial_helper_t
-	{
-		___null_t _{};
-	};
 }	 // namespace serek
