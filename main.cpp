eff1a1e42bb3c33a87476fddbd40e93f89174ecf
raidgar98 <raidgar98@onet.pl>
2021-08-24 19:14:58 +0200

    extended usecases

--- main.cpp
+++ main.cpp
@@ -4,26 +4,41 @@
 
 #include <boost/type_index.hpp>
 
+struct print_visitor
+{
+
+};
+
 struct DUPA
 {
 	int dudud;
 };
 
-struct dupa : public serek::serial_helper_t
+struct dupa 
 {
-	serek::field<&dupa::_, int> x;
-	serek::field<&dupa::x, DUPA> y;
+	serek::ffield<int> gg;
+	serek::field<&dupa::gg, DUPA> y{};
+	serek::field<&dupa::y, float> z{2.67f};
 };
 
 int main()
 {
-	dupa x{{}, 2};
+	dupa x{ .gg{2} };
+	std::cout << x.gg << std::endl;
 
-	using x_x_t = decltype(x.x);
+	using x_x_t = decltype(x.gg);
 	using x_y_t = decltype(x.y);
 
 	std::cout << boost::typeindex::type_id<x_x_t>().pretty_name() << std::endl;
 	std::cout << boost::typeindex::type_id<x_y_t>().pretty_name() << std::endl;
+	std::cout << boost::typeindex::type_id<decltype(x.z)>().pretty_name() << std::endl;
+
+	std::cout << x.gg + 20 << std::endl;
+	std::cout << (x.gg < 1) << std::endl;
+	std::cout << (20 - x.gg) << std::endl;
+	std::cout << x.gg << std::endl;
+	
+	std::cout << x.z << std::endl;
 
 
 	return 0;
