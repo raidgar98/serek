d296bd95aa6d7e4a0ea2db5038e861c340fae390
raidgar98 <raidgar98@onet.pl>
2021-08-24 19:13:56 +0200

    fixed adding of compiler options

--- cmake/add_compiler_flags.cmake
+++ cmake/add_compiler_flags.cmake
@@ -1,7 +1,9 @@
 MACRO(add_compiler_flags)
 
 	set(variadic ${ARGN})
-	list(LENGTH variadic var_length)
-	set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} ${variadic})
+	foreach(var ${variadic})
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${var}")
+	endforeach()
+	message("current compile command flags: ${CMAKE_CXX_FLAGS}")
 
 ENDMACRO()
