f7a6f1cb623f5ef87b46235bf61b6a4c794cdd9a
raidgar98 <raidgar98@onet.pl>
2021-08-24 19:15:39 +0200

    :sparkles: Implement fundamental type holder

--- include/serek/private/fundamental_type_holder.hpp
+++ include/serek/private/fundamental_type_holder.hpp
@@ -12,17 +12,82 @@ namespace serek
 		{
 			T value;
 
-			operator T&() { return get(); }
+			// conversion
+			operator T() { return get(); }
+			operator T() const { return get(); }
 
+			// access operator
+			T operator()(){ return get(); }
+			T operator()() const { return get(); }
+
+			// forward all constructors
 			template<typename ... other_types>
-			fundamental_type_holder(other_types&& ... argv) : value{ std::forward<other_types>(argv)... } {}
+			fundamental_type_holder(other_types ... argv) : value{ std::forward<other_types>(argv)... } {}
 
+			// comprasion operators
 			template<typename other_t>
-			inline friend auto operator<=>(const fundamental_type_holder me, const other_t& other) { return me.value <=> other; }
+			inline friend auto operator<=>(fundamental_type_holder me, other_t other) { return me.value <=> other; }
+
+			// assigment operators
+				// template<typename other_t> inline friend auto operator*=(fundamental_type_holder me, other_t other){ return me.value *= other; }
+				// template<typename other_t> inline friend auto operator/=(fundamental_type_holder me, other_t other){ return me.value /= other; }
+				// template<typename other_t> inline friend auto operator+=(fundamental_type_holder me, other_t other){ return me.value += other; }
+				// template<typename other_t> inline friend auto operator-=(fundamental_type_holder me, other_t other){ return me.value -= other; }
+				// template<typename other_t> inline friend auto operator%=(fundamental_type_holder me, other_t other){ return me.value %= other; }
+				// template<typename other_t> inline friend auto operator<<=(fundamental_type_holder me, other_t other){ return me.value <<= other; }
+				// template<typename other_t> inline friend auto operator>>=(fundamental_type_holder me, other_t other){ return me.value >>= other; }
+				// template<typename other_t> inline friend auto operator&=(fundamental_type_holder me, other_t other){ return me.value &= other; }
+				// template<typename other_t> inline friend auto operator^=(fundamental_type_holder me, other_t other){ return me.value ^= other; }
+				// template<typename other_t> inline friend auto operator|=(fundamental_type_holder me, other_t other){ return me.value |= other; }
+
+				// template<typename other_t> inline friend auto operator*=(other_t other, fundamental_type_holder me){ return other *= me.value; }
+				// template<typename other_t> inline friend auto operator/=(other_t other, fundamental_type_holder me){ return other /= me.value; }
+				// template<typename other_t> inline friend auto operator+=(other_t other, fundamental_type_holder me){ return other += me.value; }
+				// template<typename other_t> inline friend auto operator-=(other_t other, fundamental_type_holder me){ return other -= me.value; }
+				// template<typename other_t> inline friend auto operator%=(other_t other, fundamental_type_holder me){ return other %= me.value; }
+				// template<typename other_t> inline friend auto operator<<=(other_t other, fundamental_type_holder me){ return other <<= me.value; }
+				// template<typename other_t> inline friend auto operator>>=(other_t other, fundamental_type_holder me){ return other >>= me.value; }
+				// template<typename other_t> inline friend auto operator&=(other_t other, fundamental_type_holder me){ return other &= me.value; }
+				// template<typename other_t> inline friend auto operator^=(other_t other, fundamental_type_holder me){ return other ^= me.value; }
+				// template<typename other_t> inline friend auto operator|=(other_t other, fundamental_type_holder me){ return other |= me.value; }
+
+			// increment/decrement operators
+				inline friend auto operator++(fundamental_type_holder me){ return ++me.value; }
+				inline friend auto operator--(fundamental_type_holder me){ return ++me.value; }
+				inline friend auto operator++(fundamental_type_holder me, int){ return me.value++; }
+				inline friend auto operator--(fundamental_type_holder me, int){ return me.value++; }
+
+			// arithmetic operators
+				// single argument
+					inline friend auto operator+(fundamental_type_holder me){ return +me.value; }
+					inline friend auto operator-(fundamental_type_holder me){ return -me.value; }
+					inline friend auto operator~(fundamental_type_holder me){ return ~me.value; }
+				// double arguments
+					// template<typename other_t> inline friend auto operator+(fundamental_type_holder me, other_t other) { return me.value + other; }
+					// template<typename other_t> inline friend auto operator-(fundamental_type_holder me, other_t other) { return me.value - other; }
+					// template<typename other_t> inline friend auto operator*(fundamental_type_holder me, other_t other) { return me.value * other; }
+					// template<typename other_t> inline friend auto operator/(fundamental_type_holder me, other_t other) { return me.value / other; }
+					// template<typename other_t> inline friend auto operator%(fundamental_type_holder me, other_t other) { return me.value % other; }
+					// template<typename other_t> inline friend auto operator&(fundamental_type_holder me, other_t other) { return me.value & other; }
+					// template<typename other_t> inline friend auto operator|(fundamental_type_holder me, other_t other) { return me.value | other; }
+					// template<typename other_t> inline friend auto operator^(fundamental_type_holder me, other_t other) { return me.value ^ other; }
+					// template<typename other_t> inline friend auto operator<<(fundamental_type_holder me, other_t other) { return me.value << other; }
+					// template<typename other_t> inline friend auto operator>>(fundamental_type_holder me, other_t other) { return me.value >> other; }
+
+					// template<typename other_t> inline friend auto operator+(other_t other, fundamental_type_holder me) { return other + me.value; }
+					// template<typename other_t> inline friend auto operator-(other_t other, fundamental_type_holder me) { return other - me.value; }
+					// template<typename other_t> inline friend auto operator*(other_t other, fundamental_type_holder me) { return other * me.value; }
+					// template<typename other_t> inline friend auto operator/(other_t other, fundamental_type_holder me) { return other / me.value; }
+					// template<typename other_t> inline friend auto operator%(other_t other, fundamental_type_holder me) { return other % me.value; }
+					// template<typename other_t> inline friend auto operator&(other_t other, fundamental_type_holder me) { return other & me.value; }
+					// template<typename other_t> inline friend auto operator|(other_t other, fundamental_type_holder me) { return other | me.value; }
+					// template<typename other_t> inline friend auto operator^(other_t other, fundamental_type_holder me) { return other ^ me.value; }
+					// template<typename other_t> inline friend auto operator<<(other_t other, fundamental_type_holder me) { return other << me.value; }
+					// template<typename other_t> inline friend auto operator>>(other_t other, fundamental_type_holder me) { return other >> me.value; }
 
 		private:
-			T& get() { return value; }
-			const T& get() const { return value; }
+			inline constexpr T& get() { return value; }
+			inline constexpr const T& get() const { return value; }
 
 			static_assert( std::is_fundamental_v<T> );
 		};
