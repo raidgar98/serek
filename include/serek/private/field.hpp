e4b915753bce05b96363a81efa84cdace9cae547
raidgar98 <raidgar98@onet.pl>
2021-08-24 19:16:35 +0200

    preimplemented filed

--- include/serek/private/field.hpp
+++ include/serek/private/field.hpp
@@ -12,7 +12,10 @@
 #pragma once
 
 #include <concepts>
+#include <stdexcept>
 #include "fundamental_type_holder.hpp"
+#include "visitors.hpp"
+
 namespace serek
 {
 	namespace detail
@@ -24,18 +27,100 @@ namespace serek
 			requires !std::is_final_v<T>;
 		};
 
-		template<auto ref_to_prev_member, typename T> struct field_impl;
-
-		template<typename owner_t, typename previous_field_t, previous_field_t owner_t::*value,
-					field_impl_field_t_req current_field_t>
-		struct field_impl<value, current_field_t> : public current_field_t
+		template<typename T>
+		concept fundamental_req = requires
 		{
+			requires std::is_fundamental_v<T>;
 		};
+
+		using visitor_result_t = visitors::result_t;
+		inline constexpr visitor_result_t get_end_visitor_result() { return false; }
+
+		template<typename T>
+		concept acceptor_worker_req = requires(T x)
+		{
+			std::same_as<decltype(x.result), visitor_result_t>;
+		};
+
+		struct acceptor_base
+		{
+			visitor_result_t result{};
+
+		 protected:
+			template<visitors::visitor_req visitor_t>
+			void validate_visitator(visitor_t* visitor) const
+			{
+				if(visitor == nullptr) throw std::invalid_argument{"visitor cannot be nullptr"};
+				if(visitor->that == nullptr)
+					throw std::invalid_argument{"that in visitor cannot be nullptr"};
+			}
+		};
+
+		struct basic_acceptor_worker : public acceptor_base
+		{
+			template<visitors::visitor_req visitor_t>
+			explicit basic_acceptor_worker(visitor_t* visitor)
+			{
+				validate_visitator(visitor);
+				result = visitor->visit(this);
+			}
+		};
+		template<typename class_t, auto value>
+		struct forward_acceptor_worker : public acceptor_base
+		{
+			template<visitors::visitor_req visitor_t>
+			explicit forward_acceptor_worker(visitor_t* visitor)
+			{
+				validate_visitator(visitor);
+				visitor->last_result = visitors::visit(
+					visitor, 
+					reinterpret_cast<class_t*>(visitor->that)->*value
+				);
+				result = visitor->visit(this);
+			}
+		};
+
+		template<acceptor_worker_req acceptor_worker_t> struct acceptor_core_impl
+		{
+			template<visitors::visitor_req visitor_t> visitor_result_t accept(visitor_t* visitor)
+			{
+				return acceptor_worker_t{visitor}.result;
+			}
+
+			template<visitors::visitor_req visitor_t>
+			visitor_result_t accept(visitor_t* visitor) const
+			{
+				return acceptor_worker_t{visitor}.result;
+			}
+		};
+
+		template<typename current_field_t, acceptor_worker_req acceptor_worker_t>
+		struct field_impl_value_handler;
+
+		template<field_impl_field_t_req current_field_t, acceptor_worker_req acceptor_worker_t>
+		struct field_impl_value_handler<current_field_t, acceptor_worker_t> :
+			 public current_field_t,
+			 public acceptor_core_impl<acceptor_worker_t>
+		{};
+
+		template<fundamental_req current_field_t, acceptor_worker_req acceptor_worker_t>
+		struct field_impl_value_handler<current_field_t, acceptor_worker_t> :
+			 public detail::fundamental_type_holder<current_field_t>,
+			 public acceptor_core_impl<acceptor_worker_t>
+		{};
+
+		template<auto ref_to_prev_member, typename T> struct field_impl;
+		template<typename owner_t, typename previous_field_t, previous_field_t owner_t::*value, typename current_field_t>
+		struct field_impl<value, current_field_t> :
+			 public field_impl_value_handler<current_field_t, forward_acceptor_worker<owner_t, value>>
+		{};
+
 	}	 // namespace detail
+
 	template<auto value, typename current_field_t>
-	using field = typename std::conditional<
-		 std::is_fundamental_v<current_field_t>,
-		 detail::field_impl<value, detail::fundamental_type_holder<current_field_t>>,
-		 detail::field_impl<value, current_field_t>>::type;
+	using field = detail::field_impl<value, current_field_t>;
+
+	template<typename first_field_t>
+	using ffield = detail::field_impl_value_handler<first_field_t, typename detail::basic_acceptor_worker>;
 
 }	 // namespace serek
