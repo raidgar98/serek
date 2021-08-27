72e657ec03fdf29de09471f42bea505d38f9a390
raidgar98 <raidgar98@onet.pl>
2021-08-27 21:32:43 +0200

    :sparkles: Implement exception_base

---
+++ include/serek/private/exceptions.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdexcept>
+#include <memory>
+
+#include <serek/private/types.hpp>
+
+namespace serek
+{
+	namespace exceptions
+	{
+		struct exception_base : public std::exception
+		{
+			exception_base(const char* msg = "unknown");
+
+			virtual const char* what() const noexcept;
+			virtual std::string pretty() const noexcept;
+			inline friend std::ostream& operator<<(std::ostream& os, const exception_base& ex)
+			{
+				return os << ex.pretty();
+			}
+
+		 private:
+			std::unique_ptr<std::string> message;
+			std::unique_ptr<std::string> stacktrace;
+		};
+	}	 // namespace exceptions
+}	 // namespace serek
--- src/serek.cpp
+++ src/serek.cpp
@@ -1,1 +1,42 @@
+#include <sstream>
+
 #include <serek/serek.hpp>
+
+#ifdef BOOST_STACKTRACE_USE_BACKTRACE
+
+	#include <boost/stacktrace.hpp>
+
+namespace
+{
+	void save_stacktrace(std::unique_ptr<std::string>& out)
+	{
+		const auto frames{boost::stacktrace::stacktrace().as_vector()};
+		std::stringstream stream{};
+		stream << boost::stacktrace::stacktrace();
+		out = std::make_unique<std::string>(stream.str());
+	}
+}	 // namespace
+#else
+void save_stacktrace(std::string& out) {}
+#endif
+
+serek::exceptions::exception_base::exception_base(const char* msg) :
+	 message{std::make_unique<std::string>(msg)}
+{
+	save_stacktrace(this->stacktrace);
+}
+
+std::string serek::exceptions::exception_base::pretty() const noexcept
+{
+	std::stringstream ss;
+	ss << "exception message: " << this->what() << "\n"
+		<< "\n";
+	if(this->stacktrace) ss << "stacktrace:\n" << *this->stacktrace << '\n';
+
+	return ss.str();
+}
+
+const char* serek::exceptions::exception_base::what() const noexcept
+{
+	return this->message->c_str();
+}
