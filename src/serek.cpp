da7b8e421825ab46c390d09f227e154acfbd9e77
raidgar98 <raidgar98@onet.pl>
2021-08-28 18:41:20 +0200

    :sparkles: Implement gathering of stacktrace

--- src/serek.cpp
+++ src/serek.cpp
@@ -4,36 +4,59 @@
 
 #ifdef BOOST_STACKTRACE_USE_BACKTRACE
 
+	#include <iomanip>
 	#include <boost/stacktrace.hpp>
 
 namespace
 {
+
+	inline constexpr size_t STACKTRACE_OFFSET{4};
+
 	void save_stacktrace(std::unique_ptr<serek::str>& out)
 	{
-		const auto frames{boost::stacktrace::stacktrace().as_vector()};
 		std::stringstream stream{};
-		stream << boost::stacktrace::stacktrace();
-		out = std::make_unique<std::string>(stream.str());
+		const auto& frames{boost::stacktrace::stacktrace().as_vector()};
+		for(size_t i = STACKTRACE_OFFSET; i < frames.size(); ++i)
+		{
+			const auto& frame		  = frames[i];
+			const int64_t frame_id = static_cast<int64_t>(i - STACKTRACE_OFFSET);
+			stream << "\t#" << std::setw(4) << std::setfill('0') << frame_id;
+			if(frame.empty()) stream << "\tunresolved\n";
+			else
+			{
+				stream << "\tat '" << frame.name() << '\'';
+
+				if(frame.source_line())
+					stream << " in file " << frame.source_file() << ':' << frame.source_line() << '\n';
+				else	 // filename and line number unresolved
+					stream << '\n';
+			}
+		}
+		out = std::make_unique<serek::str>(stream.str());
 	}
 }	 // namespace
 #else
 void save_stacktrace(str& out) {}
 #endif
 
-	exceptions::exception_base::exception_base(str_v msg) : message{std::make_unique<str>(msg)}
+namespace serek
 {
-	save_stacktrace(this->stacktrace);
-}
+
+	exceptions::exception_base::exception_base(str_v msg) : message{std::make_unique<str>(msg)}
+	{
+		save_stacktrace(this->stacktrace);
+	}
 
 	str exceptions::exception_base::pretty() const noexcept
-{
-	std::stringstream ss;
-	ss << "exception message: " << this->what() << "\n"
-		<< "\n";
-	if(this->stacktrace) ss << "stacktrace:\n" << *this->stacktrace << '\n';
+	{
+		std::stringstream ss;
+		ss << "exception message: " << this->what() << "\n\n";
+		if(this->stacktrace) ss << "stacktrace:\n\n" << *this->stacktrace << '\n';
+		return ss.str();
+	}
 
-	return ss.str();
-}
+	const char* exceptions::exception_base::what() const noexcept { return this->message->c_str(); }
+}	 // namespace serek
 
 std::ostream& operator<<(std::ostream& os, const serek::exceptions::exception_base& ex)
 {
