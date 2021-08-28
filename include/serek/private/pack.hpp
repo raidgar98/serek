6e2f0d7a74bb839357076339f17356b3ec0a0146
raidgar98 <raidgar98@onet.pl>
2021-08-28 19:57:32 +0200

    :boom::sparkles: Introduce pack which is class wrapper

---
+++ include/serek/private/pack.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+
+#include <serek/private/field.hpp>
+
+namespace serek
+{
+	namespace detail
+	{
+		template<auto last_field> struct pack_impl;
+
+		template<typename owner_t, typename previous_field_t, previous_field_t owner_t::*value>
+		struct pack_impl<value> : public owner_t
+		{
+			template<reqs::visitor_req visitor_t> visitor_result_t visit(visitor_t* v)
+			{
+				void* i_prev = v->that;
+				v->that		 = reinterpret_cast<void*>(this);
+				const visitor_result_t result
+					 = visitors::visit(v, &(dynamic_cast<owner_t*>(this)->*value));
+				v->that = i_prev;
+				return result;
+			}
+		};
+	}	 // namespace detail
+
+	template<auto last_field> using pack = typename detail::pack_impl<last_field>;
+}	 // namespace serek
--- include/serek/serek.hpp
+++ include/serek/serek.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <serek/private/exceptions.hpp>
-#include <serek/private/field.hpp>
+#include <serek/private/pack.hpp>
 
 namespace serek
 {
