00e0cbd82117ea323d15cf435da4460c8c53e0f6
raidgar98 <raidgar98@onet.pl>
2021-08-28 18:38:16 +0200

    :safety_vest: Add custom assertion

--- include/serek/private/exceptions.hpp
+++ include/serek/private/exceptions.hpp
@@ -20,6 +20,17 @@ namespace serek
 			std::unique_ptr<str> message;
 			std::unique_ptr<str> stacktrace;
 		};
+
+		struct assert_exception : public exception_base
+		{
+			using exception_base::exception_base;
+		};
 		};
 	}	 // namespace exceptions
+
+	template<typename exception_t = typename exceptions::assert_exception>
+	void require(const bool result, const str_v error_message = "failed on check!")
+	{
+		if(!result) throw exception_t{error_message};
+	}
 }	 // namespace serek
