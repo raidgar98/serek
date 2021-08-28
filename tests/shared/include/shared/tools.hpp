949bf1a1c169ff0f39e4051ca1ee1b64a10b9c63
raidgar98 <raidgar98@onet.pl>
2021-08-28 23:10:55 +0200

    :construction: Add shared tools, for future shared utilities

---
+++ tests/shared/include/shared/tools.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <serek/serek.hpp>
+#include <boost/ut.hpp>
+
+namespace serek
+{
+	namespace tests
+	{
+	}	 // namespace tests
+}	 // namespace serek
+
+namespace but = boost::ut;
+using namespace but::literals;
+
+int main();
---
+++ tests/shared/tools.cpp
@@ -0,0 +1,10 @@
+#include <shared/tools.hpp>
+
+namespace serek
+{
+	namespace tests
+	{
+	}
+}	 // namespace serek
+
+int main() {}
