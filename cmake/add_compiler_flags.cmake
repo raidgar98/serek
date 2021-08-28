2e53794bc26dfc2768776495b5245e6d16d18555
raidgar98 <raidgar98@onet.pl>
2021-08-28 18:32:04 +0200

    :mute: Removed debug logging from cmake

--- cmake/add_compiler_flags.cmake
+++ cmake/add_compiler_flags.cmake
@@ -4,6 +4,5 @@ MACRO(add_compiler_flags)
 	foreach(var ${variadic})
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${var}")
 	endforeach()
-	message("current compile command flags: ${CMAKE_CXX_FLAGS}")
 
 ENDMACRO()
