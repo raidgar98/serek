72a5693fbd9903b0897f5847ae8783efa3873552
raidgar98 <raidgar98@onet.pl>
2021-08-29 23:30:01 +0200

    📝 added documentation for exceptions

--- include/serek/private/exceptions.hpp
+++ include/serek/private/exceptions.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file exceptions.hpp
+ * @author Krzysztof Mochocki (raidgar98@onet.pl)
+ * @brief Contains declarations of exception mechanism
+ * @version 0.1
+ * @date 2021-08-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #pragma once
 
 #include <memory>
@@ -11,13 +22,35 @@
 
 namespace serek
 {
+	/**
+	 * @brief Contains declarations and definitions of exception structs
+	 */
 	namespace exceptions
 	{
+		/**
+		 * @brief base class for exceptions
+		 */
 		struct exception_base : public std::exception
 		{
-			exception_base(str_v msg = "unknown");
+			/**
+			 * @brief Construct a new exception base object
+			 * 
+			 * @param msg message to print on ::what()
+			 */
+			explicit exception_base(str_v msg = "unknown");
 
+			/**
+			 * @brief accessor to @ref message
+			 * 
+			 * @return const char* 
+			 */
 			virtual const char* what() const noexcept;
+
+			/**
+			 * @brief returns ::what() with stacktrace
+			 * 
+			 * @return str 
+			 */
 			virtual str pretty() const noexcept;
 
 		 private:
@@ -25,27 +58,57 @@ namespace serek
 			std::shared_ptr<str> stacktrace;
 		};
 
+		/**
+		 * @brief most common exception throw
+		 */
 		struct assert_exception : public exception_base
 		{
 			using exception_base::exception_base;
 		};
 
+		/**
+		 * @brief should be thrown when given pointer is set to null, when non-null pointer is required
+		 */
 		struct pointer_was_null_exception : public exception_base
 		{
 			using exception_base::exception_base;
 		};
 
-		struct comprasion_fail_exception : public exception_base
+		/**
+		 * @brief same as assert exception, but additionally prints additional info about compared values (values and comprasion functor)
+		 */
+		struct comprasion_fail_exception : public assert_exception
 		{
-			explicit comprasion_fail_exception(const str_v i_what, const str& i_comprasion_result = str{}) : exception_base{i_what}, comprasion_result{new str{i_comprasion_result}} {}
+			/**
+			 * @brief Construct a new comprasion fail exception object
+			 * 
+			 * @param i_what forwarded to parent (what())
+			 * @param i_comprasion_result string to set as comprasion result
+			 */
+			explicit comprasion_fail_exception(const str_v i_what, const str& i_comprasion_result = str{}) : assert_exception{i_what}, comprasion_result{new str{i_comprasion_result}} {}
 
+			/**
+			 * @brief overrides default pretty, by preinserting @ref comprasion_result
+			 * 
+			 * @return str 
+			 */
 			virtual str pretty() const noexcept override;
 
 		 private:
 			std::shared_ptr<str> comprasion_result;
 		};
+
 		namespace
 		{
+			/**
+			 * @brief generates message of failed comprasion
+			 * 
+			 * @tparam operator_t functor type
+			 * @tparam T type of element that was compared
+			 * @param l_value left operand
+			 * @param r_value right operand
+			 * @return str 
+			 */
 			template<typename operator_t, typename T>
 			str format_fail_comprasion(const T& l_value, const T& r_value)
 			{
@@ -53,15 +116,31 @@ namespace serek
 				ss << "comprasion via functor: " << boost::typeindex::template type_id<operator_t>().pretty_name() << "{}( `" << l_value << "`, `" << r_value << "` ) returned false!";
 				return ss.str();
 			}
-		}
-	}	 // namespace exceptions
+		}	 // namespace detail
+	}		 // namespace exceptions
 
+	/**
+	 * @brief alternative to assert
+	 * 
+	 * @tparam exceptions::assert_exception exception type to throw
+	 * @param result result from some action
+	 * @param error_message message to forwrd to exception
+	 */
 	template<reqs::throwable_req exception_t = typename exceptions::assert_exception>
 	void require(const bool result, const str_v error_message = "failed on check!")
 	{
 		if(!result) throw exception_t{error_message};
 	}
 
+	/**
+	 * @brief overload of above, allows additionally print given values
+	 * 
+	 * @tparam operator_t functor that will be used for comprasion ( @see std::less )
+	 * @tparam T type of compared elements
+	 * @param l_value left operand
+	 * @param r_value right operand
+	 * @param error_message message to forwrd to exception
+	 */
 	template<template<typename T> typename operator_t, typename T>
 	void require(const T& l_value, const T& r_value, const str_v error_message = "comprasion failed!")
 	{
@@ -69,11 +148,25 @@ namespace serek
 		if(!operator_with_type{}(l_value, r_value)) throw typename exceptions::comprasion_fail_exception{error_message, exceptions::template format_fail_comprasion<operator_with_type>(l_value, r_value)};
 	}
 
+	/**
+	 * @brief overload of above, specialized for checking pointers
+	 * 
+	 * @tparam pointer_t type of pointer to validate
+	 * @param ptr pointer to check
+	 * @param error_message message to forwrd to exception
+	 */
 	template<reqs::comparable_as_pointer_req pointer_t>
-	void require(const pointer_t& result, const str_v error_message = "given pointer was null!")
+	void require(const pointer_t& ptr, const str_v error_message = "given pointer was null!")
 	{
-		if(result == nullptr) throw exceptions::pointer_was_null_exception{error_message};
+		if(ptr == nullptr) throw exceptions::pointer_was_null_exception{error_message};
 	}
 }	 // namespace serek
 
+/**
+ * @brief operator to easly print exceptions
+ * 
+ * @param os stream
+ * @param ex exception
+ * @return std::ostream& 
+ */
 std::ostream& operator<<(std::ostream& os, const serek::exceptions::exception_base& ex);
