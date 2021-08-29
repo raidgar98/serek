6eae57eb97b32507e22c0958efed2c895e509059
raidgar98 <raidgar98@onet.pl>
2021-08-29 16:27:59 +0200

    :book: added docs for interface file (serek.hpp)

--- include/serek/serek.hpp
+++ include/serek/serek.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file serek.hpp
+ * @author Krzysztof Mochocki (raidgar98@onet.pl)
+ * @brief Interface for other programmers
+ * @version 0.1
+ * @date 2021-08-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #pragma once
 
 #include <serek/private/pack.hpp>
