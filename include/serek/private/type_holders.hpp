189e0ebb26dae87b2e9c112df15fa4a6c5d27fa9
raidgar98 <raidgar98@onet.pl>
2021-08-30 20:25:02 +0200

    📝 Add documentation for type_holders

--- include/serek/private/type_holders.hpp
+++ include/serek/private/type_holders.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file type_holders.hpp
+ * @author Krzysztof Mochocki (raidgar98@onet.pl)
+ * @brief Contatins declarations and definitions of structures responsible for accessing contained value
+ * @version 0.1
+ * @date 2021-08-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #pragma once
 
 #include <utility>
@@ -7,18 +18,36 @@ namespace serek
 {
 	namespace detail
 	{
+		/**
+		 * @brief fasade that derivates from given type and forwards all constructor to given type
+		 * 
+		 * @tparam T type to derive from
+		 */
 		template<typename T>
 		struct type_holder : public T
 		{
+			/**
+			 * @brief Construct a new type holder object by forwarding all values to parent
+			 * 
+			 * @tparam other_types variadic types
+			 * @param argv variadic arguments
+			 */
 			template<typename... other_types>
 			type_holder(other_types&&... argv) : T{std::forward<other_types>(argv)...}
 			{
 			}
 		};
 
+		/**
+		 * @brief wrapper for fundamental types
+		 * @note forward all possible operators and providec _implicit_ conversions
+		 * 
+		 * @tparam T fundamental type to wrap
+		 */
 		template<typename T>
 		struct fundamental_type_holder
 		{
+			/** @brief type for internal library usage */
 			using _____fundamental_type_wrapper = std::true_type;
 			using value_t								= T;
 
@@ -103,10 +132,11 @@ namespace serek
 			// template<typename other_t> inline friend auto operator<<(other_t other, fundamental_type_holder me) { return other << me.value; }
 			// template<typename other_t> inline friend auto operator>>(other_t other, fundamental_type_holder me) { return other >> me.value; }
 
-		 private:
+		private:
 			inline constexpr T& get() { return value; }
 			inline constexpr const T& get() const { return value; }
 
+			// verify is given type is fundamental one more time
 			static_assert(std::is_fundamental_v<T>);
 		};
 	}	 // namespace detail
