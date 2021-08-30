b0dc0e7f1ada601b41d588a5b15860ea195fbc35
raidgar98 <raidgar98@onet.pl>
2021-08-30 20:25:32 +0200

    📝 Add documenation for types

--- include/serek/private/types.hpp
+++ include/serek/private/types.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file types.hpp
+ * @author Krzysztof Mochocki (raidgar98@onet.pl)
+ * @brief Contains aliases for commonly used types
+ * @version 0.1
+ * @date 2021-08-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #pragma once
 
 #include <string>
