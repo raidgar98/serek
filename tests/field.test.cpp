3b2671d9d16127233a85e3a88484bba2d9cfe734
raidgar98 <raidgar98@onet.pl>
2021-09-07 21:03:03 +0200

    🚚 Move test classes to separarte file

--- tests/field.test.cpp
+++ tests/field.test.cpp
@@ -1,63 +1,12 @@
 #include <shared/tools.hpp>
+#include <shared/struct_set.hpp>
 
 namespace
 {
 	namespace but = boost::ut;
 	using namespace but::literals;
 	using namespace serek::tests;
-
-	using serek::ffield;
-	using serek::field;
-
-	struct test_struct_0
-	{
-		ffield<int> field_0;
-	};
-
-	struct test_struct_1
-	{
-		ffield<int> field_0;
-		field<&test_struct_1::field_0, float> field_1;
-	};
-
-	struct test_struct_2 : public test_struct_1
-	{
-		field<&test_struct_2::field_1, char> field_2;
-	};
-
-	struct test_struct_3
-	{
-		ffield<serek::str> field_0;
-		inline friend std::ostream& operator<<(std::ostream& os, const test_struct_3& str) { return os << str.field_0; }
-	};
-
-	struct test_struct_4
-	{
-		ffield<test_struct_3> field_0;
-	};
-
-	struct test_struct_5
-	{
-		ffield<int> field_0;
-		int empty_field_1;
-		field<&test_struct_5::field_0, float> field_2;
-	};
-
-	struct test_struct_6
-	{
-		ffield<number_t<0>> field_0;
-		field<&test_struct_6::field_0, number_t<1>> field_1;
-		field<&test_struct_6::field_1, number_t<2>> field_2;
-		field<&test_struct_6::field_2, number_t<3>> field_3;
-		field<&test_struct_6::field_3, number_t<4>> field_4;
-	};
-
-	struct test_struct_7
-	{
-		ffield<int> field_0;
-		field<&test_struct_7::field_0, int> field_1;
-		field<&test_struct_7::field_0, int> field_2;
-	};
+	using namespace example_structs;
 
 	struct trivial_serializer : public serek::visitors::base_visitor_members
 	{
