bb941591e7a604c741252368af9b72c79b13c313
raidgar98 <raidgar98@onet.pl>
2021-09-07 22:30:18 +0200

    📝 Add missing docs for requirements

--- include/serek/private/requirements.hpp
+++ include/serek/private/requirements.hpp
@@ -45,6 +45,11 @@ namespace serek
 				requires std::is_fundamental_v<T>;
 			};
 
+			/**
+			 * @brief checks is given type is fundamental wrapper
+			 * 
+			 * @tparam T type to check
+			 */
 			template<typename T>
 			concept fundamental_wrapper_req = requires
 			{
@@ -52,6 +57,11 @@ namespace serek
 				typename T::value_t;
 			};
 
+			/**
+			 * @brief checks is given type fullfils visitor requirements
+			 * 
+			 * @tparam visitor_t type to check
+			 */
 			template<typename visitor_t>
 			concept visitor_req = requires(visitor_t* x)
 			{
@@ -61,8 +71,14 @@ namespace serek
 				{x->that(static_cast<void*>(NULL))};
 			};
 
+			/**
+			 * @brief encapsulates types required to define `acceptor_req`
+			 */
 			namespace acceptor_req_details
 			{
+				/**
+				 * @brief fullfills visitor_req
+				 */
 				struct ex_vis
 				{
 					visitor_result_t last_result;
@@ -83,14 +99,28 @@ namespace serek
 
 			}	 // namespace acceptor_req_details
 
+			/**
+			 * @brief checks is given type can be used as accptor
+			 * 
+			 * @tparam acceptor_t 
+			 */
 			template<typename acceptor_t>
 			concept acceptor_req = requires(acceptor_t* x)
 			{
 				std::same_as<decltype(x->template visit(new acceptor_req_details::ex_vis)), visitor_result_t>;
 			};
 
+			/**
+			 * @brief encapsulates helper types to define `acceptor_worker_req`
+			 */
 			namespace acceptor_worker_req_detail
 			{
+				/**
+				 * @brief own imlementation of std::is_invocable
+				 * 
+				 * @tparam T type to check is invocable
+				 * @tparam Argv invocable argument types
+				 */
 				template<typename T, typename... Argv>
 				concept acceptor_worker_req_helper = requires(T x, Argv && ... args)
 				{
@@ -98,6 +128,11 @@ namespace serek
 				};
 			}	 // namespace acceptor_worker_req_detail
 
+			/**
+			 * @brief checks is given type can be used as acceptor worker
+			 * 
+			 * @tparam T type to check
+			 */
 			template<typename T>
 			concept acceptor_worker_req = requires
 			{
@@ -105,12 +140,23 @@ namespace serek
 			}
 			&&acceptor_worker_req_detail::acceptor_worker_req_helper<T, typename T::value_t*, acceptor_req_details::ex_vis*> || acceptor_worker_req_detail::acceptor_worker_req_helper<T, const typename T::value_t*, acceptor_req_details::ex_vis*>;
 
+			/**
+			 * @brief checks is given type can be compared like pointerr
+			 * 
+			 * @tparam T type to check
+			 */
 			template<typename T>
 			concept comparable_as_pointer_req = requires(T x)
 			{
 				std::same_as<decltype(x == nullptr), bool>;
 			};
 
+			/**
+			 * @brief checks is given type can be used as exception
+			 * @note it just need to be constructible from std::string_view
+			 * 
+			 * @tparam T type to check
+			 */
 			template<typename T>
 			concept throwable_req = requires
 			{
