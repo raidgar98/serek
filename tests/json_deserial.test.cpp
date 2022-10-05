7ea3b63ebb97a8e8c3747f5e1aefb9d6b003b758
raidgar98 <raidgar98@onet.pl>
2022-10-05 16:26:25 +0200

    ✅ ✨ Add detection of number with tests

--- tests/json_deserial.test.cpp
+++ tests/json_deserial.test.cpp
@@ -113,6 +113,65 @@ namespace
 			comparison_fail_exception_check([&] { check_length("\"aaa", 5); });
 		};
 
+		"length_of_number"_test = [&] {
+			const auto check_length
+				 = [](const serek::str& str, const size_t expected_length, const size_t start = 0ul) { but::expect(but::eq(length_of_number(str, start), expected_length)); };
+
+			check_length("aaa", 0);
+			check_length(" aaa", 0, 1);
+
+			std::vector<serek::str> parametrize{{
+				 "7",
+				 "2137",
+
+				 "7.2",
+				 "21.3",
+				 "21.37",
+
+				 "2e1",
+				 "2e21",
+				 "2e-1",
+				 "2e-21",
+
+				 "2.0e1",
+				 "2.0e-1",
+				 "2.0e21",
+				 "2.0e-21",
+
+				 "2.2222222e1",
+				 "2.2222222e-1",
+				 "2.2222222e21",
+				 "2.2222222e-21",
+			}};
+
+			for(const auto& str: parametrize)
+			{
+				if(const size_t pos = str.find('e'); pos != serek::str::npos)
+				{
+					serek::str item{str};
+					item[pos] = 'E';
+					parametrize.emplace_back(item);
+				}
+			}
+
+			for(const auto& str: parametrize)
+			{
+				const size_t length{str.size()};
+				check_length(str, length);
+				check_length('-' + str, length + 1);
+
+				check_length(str + "       ", length);
+				check_length("   " + str + "       ", length, 3);
+
+				comparison_fail_exception_check([&] { check_length(str + "ee", length + 2); });
+				comparison_fail_exception_check([&] { check_length(str + "EE", length + 2); });
+				comparison_fail_exception_check([&] { check_length(str + "..", length + 2); });
+
+				comparison_fail_exception_check([&] { check_length(str + "---", length + 3); });
+				comparison_fail_exception_check([&] { check_length("---" + str, length + 3); });
+			}
+		};
+
 		"item_length"_test = [&] {
 
 		};
