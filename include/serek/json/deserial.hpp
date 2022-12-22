30cba77e0d1783848b0e0805360b5643e74762ef
raidgar98 <raidgar98@onet.pl>
2022-12-22 20:39:53 +0100

    ✨ Add first implementation of complete json deserialization

--- include/serek/json/deserial.hpp
+++ include/serek/json/deserial.hpp
@@ -9,3 +9,132 @@
  *
  */
 
+#include <iomanip>
+
+#include <serek/json/common.hpp>
+#include <serek/json/tokenization.hpp>
+#include <serek/json/visitors.hpp>
+
+namespace serek
+{
+	namespace deserial
+	{
+		namespace json
+		{
+			using namespace serek::json;
+			using namespace serek::json::visitors;
+
+			namespace detail
+			{
+				struct visitor_with_stacked_names_and_tokenized_json : public base_visitor_members_with_stack_names
+				{
+					template<typename Any>
+					visitor_with_stacked_names_and_tokenized_json(const serek::str_v json_input, Any& output) :
+						 base_visitor_members_with_stack_names{&output}, tokenized_json{tokenize_json(json_input)}
+					{
+					}
+
+					template<typename Any>
+					visitor_with_stacked_names_and_tokenized_json(json_tokenizer::result_t tokenized_json_input, Any& output) :
+						 base_visitor_members_with_stack_names{&output}, tokenized_json{tokenized_json_input}
+					{
+					}
+
+					/**
+					 * @brief Pops and returns both from tokenized json and from stack
+					 *
+					 * @param key name to pop
+					 * @return json_tokenizer::result_t data from tokenized json
+					 */
+					json_tokenizer::result_t pop_for_key(const serek::str key);
+
+				 protected:
+					json_tokenizer::result_t tokenized_json;
+				};
+
+				namespace
+				{
+					template<typename Any>
+					requires std::is_integral_v<Any>
+					void fill(Any& output, const serek::str_v value) { output = static_cast<Any>(std::atoll(value.data())); }
+
+					template<typename Any>
+					requires std::is_floating_point_v<Any>
+					void fill(Any& output, const serek::str_v value) { output = static_cast<Any>(std::atof(value.data())); }
+
+					template<reqs::string_type_req Any>
+					void fill(Any& output, const serek::str_v value)
+					{
+						serek::require<std::greater>(value.length(), 1ul);
+						serek::require<std::equal_to>(value.front(), '"');
+						serek::require<std::equal_to>(value.back(), '"');
+
+						/*
+						0	1	2	3	4	5	6		length	start_pos	count
+						"	a	a	a	a	a	"		7			1				5
+						"	a	a	"					4			1				2
+						"	"							2			1				0
+					 */
+						const auto unquoted_view = value.substr(1, value.length() - 2);
+						output						 = Any{unquoted_view.begin(), unquoted_view.end()};
+					}
+
+					// this is fundamental types deserialization
+					template<reqs::visitor_req vis_t, typename Any>
+					requires serek::requirements::fundamental_req<Any> || reqs::string_type_req<Any>
+					void deserial(visitor_with_stacked_names_and_tokenized_json& json, Any* output)
+					{
+						serek::require(output);
+
+						const auto json_value = json.pop_for_key(json.top());
+						serek::require<std::equal_to>(json_value->element_type, json_element_t::FUNDAMENTAL_TYPE);
+						fill(*output, json_value->item);
+					}
+
+					// this is object deserialization
+					template<reqs::visitor_req vis_t, auto Any>
+					void deserial(visitor_with_stacked_names_and_tokenized_json& json, serek::detail::pack_impl<Any>* output)
+					{
+						serek::require(output);
+
+						vis_t new_obj_vis{json.pop_for_key(json.top()), *output};
+						serek::visitors::visit(&new_obj_vis, output);
+					}
+
+				}	 // namespace
+			}		 // namespace detail
+
+			struct json_visitor_deserializer : public detail::visitor_with_stacked_names_and_tokenized_json
+			{
+
+				template<typename Any>
+				json_visitor_deserializer(const serek::str_v json, Any& output) : visitor_with_stacked_names_and_tokenized_json{json, output}
+				{
+				}
+
+				template<typename Any>
+				json_visitor_deserializer(json_tokenizer::result_t json, Any& output) : visitor_with_stacked_names_and_tokenized_json{json, output}
+				{
+				}
+
+				template<typename Any>
+				visitor_result_t operator()(serek::detail::type_holder<Any>* any)
+				{
+					this->process(any);
+					return true;
+				}
+
+			 private:
+				template<typename Any>
+				void process(serek::detail::type_holder<Any>* any)
+				{
+					serek::require(!this->empty(), "missing fields!");
+					if(!this->empty()) detail::deserial<json_visitor_deserializer>(static_cast<visitor_with_stacked_names_and_tokenized_json&>(*this), reinterpret_cast<Any*>(any));
+				}
+			};
+
+			static_assert(serek::reqs::visitor_req<json_visitor_deserializer>);
+
+		}	 // namespace json
+	}		 // namespace deserial
+}	 // namespace serek
--- src/json/deserialization.cpp
+++ src/json/deserialization.cpp
@@ -1,1 +1,32 @@
 #include <serek/json/deserial.hpp>
+
+namespace serek
+{
+	namespace deserial
+	{
+		namespace json
+		{
+			namespace detail
+			{
+				json_tokenizer::result_t visitor_with_stacked_names_and_tokenized_json::pop_for_key(const serek::str key)
+				{
+					// checks
+					serek::require<std::equal_to>(this->top(), key);
+					serek::require<std::equal_to>(tokenized_json->element_type, json_element_t::OBJECT_TYPE);
+					auto iterator = tokenized_json->object.find(key);
+					serek::require<std::not_equal_to>(iterator, tokenized_json->object.end());
+
+					// acquiring
+					auto result = iterator->second;
+
+					// popping
+					tokenized_json->object.erase(iterator);
+					this->pop();
+
+					//returning
+					return result;
+				}
+			}
+		}	 // namespace json
+	}		 // namespace deserial
+}	 // namespace serek
