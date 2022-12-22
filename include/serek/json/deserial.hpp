2e56c607e294ed6397957d4aca637f6ea14055eb
raidgar98 <raidgar98@onet.pl>
2022-12-22 20:04:32 +0100

    🌱 Add file for core deserialization

---
+++ include/serek/json/deserial.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file deserial.hpp
+ * @author Krzysztof Mochocki (raidgar98@onet.pl)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-21
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
---
+++ src/json/deserialization.cpp
@@ -0,0 +1,1 @@
+#include <serek/json/deserial.hpp>
